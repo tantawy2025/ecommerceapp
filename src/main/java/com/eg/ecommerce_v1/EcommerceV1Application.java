0f0486a0286de9f23eadfa210d6aa01397cd723b
tantawy <mtantawy2025@gmail.com>
2025-06-01 10:53:21 +0300

    Initial commit

---
+++ src/main/java/com/eg/ecommerce_v1/EcommerceV1Application.java
@@ -0,0 +1,13 @@
+package com.eg.ecommerce_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eg/ecommerce_v1/EcommerceV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.ecommerce_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
